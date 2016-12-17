33ed0da09a4c9ecc14a6f6fe3f7467817a0b8568
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2016-12-17 12:57:31 +0100

    integrate more test suite

--- src/tests/test_functional.py
+++ src/tests/test_functional.py
@@ -2,12 +2,34 @@ import pytest
 
 from inbome.parse import parse_inbome_header
 
-def test_example1(datadir, gpg):
-    with datadir.open("example1.mail") as fp:
+def test_rsa2048_simple(datadir, gpg):
+    with datadir.open("rsa2048-simple.eml") as fp:
         d = parse_inbome_header(fp)
-        assert d["to"] == "dkg@fifthhorseman.net"
+        assert d["to"] == "alice@testsuite.autocrypt.org"
         assert "key" in d and d["key"]
 
     gpg.import_keydata(d["key"])
 
+def test_25519_simple(datadir, gpg):
+    with datadir.open("25519-simple.eml") as fp:
+        d = parse_inbome_header(fp)
+        assert d["to"] == "alice@testsuite.autocrypt.org"
+        assert "key" in d and d["key"]
 
+    gpg.import_keydata(d["key"])
+
+def test_rsa2048_explicit_type(datadir, gpg):
+    with datadir.open("rsa2048-explicit-type.eml") as fp:
+        d = parse_inbome_header(fp)
+        assert d["to"] == "alice@testsuite.autocrypt.org"
+        assert "key" in d and d["key"]
+
+    gpg.import_keydata(d["key"])
+
+def test_rsa2048_unknown_non_critical(datadir, gpg):
+    with datadir.open("rsa2048-unknown-non-critical.eml") as fp:
+        d = parse_inbome_header(fp)
+        assert d["to"] == "alice@testsuite.autocrypt.org"
+        assert "key" in d and d["key"]
+
+    gpg.import_keydata(d["key"])
