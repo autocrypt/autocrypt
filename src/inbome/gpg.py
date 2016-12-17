d7be803cd594bf1eaea25286d878b5a02469ce37
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2016-12-17 14:04:42 +0100

    interpret whether gpg supports eddsa

--- src/inbome/gpg.py
+++ src/inbome/gpg.py
@@ -1,15 +1,32 @@
 import os
 import subprocess
 import tempfile
+import io
 
 class GPG:
     """ super basic wrapper for gpg command line invocations. """
     def __init__(self, homedir):
         self.homedir = homedir
+        self.version_info = None
 
     def _gpg(self, argv):
         args = ["gpg", "--homedir", str(self.homedir)] + argv
-        subprocess.check_call(args)
+        return subprocess.check_output(args)
+
+    def _get_version_info(self):
+        if self.version_info is None:
+            self.version_info = self._gpg(['--version'])
+        return self.version_info
+
+    def get_version(self):
+        vline = self._get_version_info().split('\n', 1)[0]
+        return vline.split(' ')[2]
+
+    def supports_eddsa(self):
+        for l in self._get_version_info().split('\n'):
+            if l.startswith('Pubkey:'):
+                return 'eddsa' in map(str.lower, map(str.strip, l.split(':', 1)[1].split(',')))
+        return False
 
     def import_keydata(self, keydata):
         with tempfile.NamedTemporaryFile(delete=False) as f:
