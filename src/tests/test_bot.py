27267057bcd0f35b5bcab6858c39575c3cdba9fe
holger krekel <holger@merlinux.eu>
2016-12-17 14:01:18 +0100

    use better bot test file

--- src/tests/test_bot.py
+++ src/tests/test_bot.py
@@ -2,7 +2,7 @@
 from inbome.bot import generate_reply, send_reply
 
 def test_generate_reply(datadir, gpg, smtpserver):
-    with datadir.open("rsa2048-simple.eml") as fp:
+    with datadir.open("rsa2048-simple-to-bot.eml") as fp:
         reply_msg = generate_reply(gpg, fp)
     assert reply_msg["To"] == "Alice <alice@testsuite.autocrypt.org>"
     assert reply_msg["From"] == "bot@autocrypt.org"
