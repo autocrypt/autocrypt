3879c5beb73ee11f6e6708d7be213c8fe7d7eb39
Azul <azul@riseup.net>
2017-04-19 15:57:28 +0200

    test: closure for test definition

--- ui/config/autocrypt-ui-test.js
+++ ui/config/autocrypt-ui-test.js
@@ -1,19 +1,21 @@
-Tests.describe('User switch', function(it, assert) {
-    var usertoggle = document.getElementById("usertoggle");
-    it.teardown = function() {
-        switchuser('alice');
-    };
-    it('starts with Alice', function() {
-        assert.content('Alice', 'username');
+(function(){
+    var describe = Tests.describe;
+    describe('User switch', function(it, assert) {
+        var usertoggle = document.getElementById("usertoggle");
+        it.teardown = function() {
+            switchuser('alice');
+        };
+        it('starts with Alice', function() {
+            assert.content('Alice', 'username');
+        });
+        it('switches to Bob', function() {
+            usertoggle.click();
+            assert.content('Bob', 'username');
+        });
+        it('switches back to Alice', function() {
+            usertoggle.click();
+            usertoggle.click();
+            assert.content('Alice', 'username');
+        });
     });
-    it('switches to Bob', function() {
-        usertoggle.click();
-        assert.content('Bob', 'username');
-    });
-    it('switches back to Alice', function() {
-        usertoggle.click();
-        usertoggle.click();
-        assert.content('Alice', 'username');
-    });
-});
-
+})();
