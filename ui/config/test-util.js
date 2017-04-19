0239fe223e968be3fd2ad125570b785be62aa613
Azul <azul@riseup.net>
2017-04-19 13:51:35 +0200

    test: separate test util and refactor

---
+++ ui/config/test-util.js
@@ -0,0 +1,58 @@
+Tests = function() {
+    var suites = {};
+    var assertions = 0;
+    var failures = 0;
+
+    function log(message) {
+        console.log( '%c ' + message, 'color: #444');
+    }
+
+    function fail(message) {
+        console.log( '%c ' + message, 'color: red');
+    }
+
+    function assert(truth, message) {
+        assertions = assertions + 1;
+        if (!truth) {
+            failures = failures + 1;
+            fail("   " + message);
+        }
+    };
+
+    assert.content = function(text, id) {
+        elem = document.getElementById(id);
+        assert( elem.innerText == text,
+                id + ' should contain "' + text + '".' );
+    };
+
+    function run() {
+        var arr = Object.entries(suites)
+        log('Running ' + arr.length + ' suites...');
+        arr.forEach(runSuite);
+        log(assertions + ' assertions. ' + failures + ' failures.');
+    };
+
+    function runSuite(suite) {
+        var name = suite[0];
+        var desc = suite[1];
+        var specs = {};
+        var it = function(behaves, fun) { specs[behaves] = fun };
+        it.setup = function() {};
+        it.teardown = function() {};
+        desc(it, assert);
+        Object.entries(specs).forEach(function (spec) {
+            var name = spec[0];
+            var task = spec[1];
+            log('  ' + name);
+            it.setup();
+            task();
+            it.teardown();
+        });
+    };
+
+    return {
+        describe: function(context, fun) { suites[context] = fun },
+        run: run
+    };
+}();
+
