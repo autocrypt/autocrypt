7968e5c1ec92ffa4e2df74fb15b0050aef6beb85
Azul <azul@riseup.net>
2017-11-06 18:03:20 +0100

    ci: fix rsync command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,5 +8,10 @@
 #   -i $TRAVIS_BUILD_DIR/id_rsa \
 #   $TRAVIS_BUILD_DIR/doc/_build ${DEPLOY_USER}@autocrypt.org:~/test
 
-rsync -a $TRAVIS_BUILD_DIR/doc/_build ${DEPLOY_USER}@autocrypt.org:test \
+rsync -avz $TRAVIS_BUILD_DIR/doc/_build/html/ \
+  ${DEPLOY_USER}@autocrypt.org:build/${TRAVIS_BRANCH/\//_} \
+  -e "ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no -i $TRAVIS_BUILD_DIR/id_rsa"
+
+rsync -avz $TRAVIS_BUILD_DIR/doc/_build/latex/autocrypt*.pdf \
+  ${DEPLOY_USER}@autocrypt.org:build/${TRAVIS_BRANCH/\//_} \
   -e "ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no -i $TRAVIS_BUILD_DIR/id_rsa"
