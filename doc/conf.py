9940353f5d5c4baf0f3413311dc01a566185691d
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2017-11-07 11:30:49 +0100

    help readers of the PDF track verbatim text across page breaks

--- doc/conf.py
+++ doc/conf.py
@@ -280,6 +280,7 @@ latex_elements = {
      # Latex figure (float) alignment
      #
      # 'figure_align': 'htbp',
+     'sphinxsetup': 'verbatimhintsturnover=true',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
