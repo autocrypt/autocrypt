255f7b6d902e8e265ef026129f3ef1b431ecad60
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2017-11-07 11:03:58 +0100

    put URLs in footnotes for pdf

--- doc/conf.py
+++ doc/conf.py
@@ -308,7 +308,7 @@ latex_show_pagerefs = True
 
 # If true, show URL addresses after external links.
 #
-latex_show_urls = True
+latex_show_urls = 'footnote'
 
 # Documents to append as an appendix to all manuals.
 #
