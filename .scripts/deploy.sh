636334de4c7a65624d87a3662fb6f87dc06c4e8d
Florian Schulze <florian.schulze@gmx.net>
2017-11-25 14:10:30 +0100

    Move ssh preparations to deployment script, so PR builds work.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+set -e
+set -u
+set -x
 # To debug your ssh connection enable this...
 # scp -r -v \
 #   -o StrictHostKeyChecking=no \
@@ -7,6 +9,13 @@
 #   -o IdentitiesOnly=yes \
 #   -i $TRAVIS_BUILD_DIR/id_rsa \
 #   $TRAVIS_BUILD_DIR/doc/_build ${DEPLOY_USER}@${DEPLOY_SERVER}:~/test
+
+mkdir -p -m 0700 ~/.ssh
+openssl aes-256-cbc -K $encrypted_ee89c1e228aa_key -iv $encrypted_ee89c1e228aa_iv -in .credentials/autocrypt.id_rsa.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+cat .credentials/autocrypt.org.hostkeys >> ~/.ssh/known_hosts
+printf "Host *\n" >> ~/.ssh/config
+printf " %sAuthentication no\n" ChallengeResponse Password KbdInteractive >> ~/.ssh/config
 
 rsync -avz $TRAVIS_BUILD_DIR/doc/_build/html/ \
   ${DEPLOY_USER}@${DEPLOY_SERVER}:build/${TRAVIS_BRANCH/\//_}
