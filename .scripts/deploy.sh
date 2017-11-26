58275f3f16bc8d263625fd1f328e654e2a767c12
Azul <azul@riseup.net>
2017-11-26 12:55:41 +0100

    deploy: only attempt if env vars are set accordingly
    This should allow builds to run on forks who do not deploy.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# only attempt to deploy if we know the secrets for the ssh key
+test $encrypted_ee89c1e228aa_key || exit 0
+test $encrypted_ee89c1e228aa_iv || exit 0
+
+# Prepare the ssh homedir.
+#
+# Decrypting the ssh private key for deploying to the server.
+# See https://docs.travis-ci.com/user/encrypting-files/ for details.
+mkdir -p -m 0700 ~/.ssh
+openssl aes-256-cbc -K $encrypted_ee89c1e228aa_key -iv $encrypted_ee89c1e228aa_iv -in .credentials/autocrypt.id_rsa.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+cat .credentials/autocrypt.org.hostkeys >> ~/.ssh/known_hosts
+printf "Host *\n" >> ~/.ssh/config
+printf " %sAuthentication no\n" ChallengeResponse Password KbdInteractive >> ~/.ssh/config
+
 # To debug your ssh connection enable this...
 # scp -r -v \
 #   -o StrictHostKeyChecking=no \
