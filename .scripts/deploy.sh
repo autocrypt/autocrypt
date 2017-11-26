4b430490aa0d747244a6dbd2f35f28e451eece21
Azul <azul@riseup.net>
2017-11-26 13:19:50 +0100

    deploy: only attempt if required env vars are set
    This will hopefully fix builds on forks.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,14 +2,17 @@
 set -e
 set -u
 set -x
-# To debug your ssh connection enable this...
-# scp -r -v \
-#   -o StrictHostKeyChecking=no \
-#   -o PasswordAuthentication=no \
-#   -o IdentitiesOnly=yes \
-#   -i $TRAVIS_BUILD_DIR/id_rsa \
-#   $TRAVIS_BUILD_DIR/doc/_build ${DEPLOY_USER}@${DEPLOY_SERVER}:~/test
 
+#Only attempt to deploy if we know the ssh key secrets, username and server
+test $encrypted_ee89c1e228aa_key
+test $encrypted_ee89c1e228aa_iv
+test $DEPLOY_USER
+test $DEPLOY_SERVER
+
+# Prepare the ssh homedir.
+#
+# Decrypting the ssh private key for deploying to the server.
+# See https://docs.travis-ci.com/user/encrypting-files/ for details.
 mkdir -p -m 0700 ~/.ssh
 openssl aes-256-cbc -K $encrypted_ee89c1e228aa_key -iv $encrypted_ee89c1e228aa_iv -in .credentials/autocrypt.id_rsa.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
@@ -17,8 +20,9 @@ cat .credentials/autocrypt.org.hostkeys >> ~/.ssh/known_hosts
 printf "Host *\n" >> ~/.ssh/config
 printf " %sAuthentication no\n" ChallengeResponse Password KbdInteractive >> ~/.ssh/config
 
+
+# Perform the actual deploy
 rsync -avz $TRAVIS_BUILD_DIR/doc/_build/html/ \
   ${DEPLOY_USER}@${DEPLOY_SERVER}:build/${TRAVIS_BRANCH/\//_}
-
 rsync -avz $TRAVIS_BUILD_DIR/doc/_build/latex/autocrypt*.pdf \
   ${DEPLOY_USER}@${DEPLOY_SERVER}:build/${TRAVIS_BRANCH/\//_}
